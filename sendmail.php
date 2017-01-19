bb804bfb834f7d42c261a0f63e0d22b3aaba6d2c
Seiji Nagashima <nagashima@k-yamako.co.jp>
2017-01-19 15:59:41 +0900

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,18 @@
+# test-com01
+<HTML>
+<HEAD><TITLE> 3GIM send Diff PRESSURE e-mail</TITLE><HEAD>
+<BODY>
+
+<H3> 3GIM Diff PRESSURE get</H3>
+<?php
+if(mail($_GET["email"], //to
+  'HI 3GIM sensor E-mail', //title
+  ' 3GIM ALARM ',"\r\n",'DATE = ',date('y-m-d').
+  '   TIME = ',date('H:i:s')."\n" .
+  'X-Mailer:PHP/'.phpVersion()))
+{ echo'<B>SUCCESS to SEND</B><BR>';}
+else
+  {  echo'<B>Fail to mb_send_mail</B><BR>';}
+?>
+
+</BODY>
